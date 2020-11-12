78709620869a444233fade9e376d55781cba7556
Thomas Lewis <tlewis@training.local>
2020-11-12 16:52:56 -0500

    test uploaded

--- View/navbar.php
+++ View/navbar.php
@@ -1,8 +1,8 @@
         <!-- Navbar -->
         <div class="navbar-grid ">
-                <a href="http://localhost/Project1/FIT-Project-FortisureFoodFront/index.php" class="nav-button0 btn btn-success">Home</a> 
+                <a href="http://localhost/FIT-Web-Course/Project1/FIT-Project-FortisureFoodFront/index.php" class="nav-button0 btn btn-success">Home</a>     
                 <a href="" class="nav-button1 btn btn-success">Shop Online</a> 
-                <a href="http://localhost/Project1/FIT-Project-FortisureFoodFront/index2.php" class="nav-button2 btn btn-success">Join Our Rewards Program!</a> 
+                <a href="http://localhost/FIT-Web-Course/Project1/FIT-Project-FortisureFoodFront/index2.php" class="nav-button2 btn btn-success">Join Our Rewards Program!</a> 
                 <a href="" class="nav-button3 btn btn-success">Store Departments</a>
             
             </div>
