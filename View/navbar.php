9eea96b0ef02bd03ad82150765740c49f29a95bf
Thomas Lewis <tlewis@training.local>
2020-12-03 15:59:01 -0500

    project 3 done

---
+++ View/navbar.php
@@ -0,0 +1,10 @@
+        <!-- Navbar -->
+        <div class="navbar-grid ">
+                <a href="./index.php" class="nav-button0 btn btn-success">Home</a>     
+                <a href="" class="nav-button1 btn btn-success">Shop Online</a> 
+                <a href="./index2.php" class="nav-button2 btn btn-success">Join Our Rewards Program!</a> 
+                <a href="./index3.php" class="nav-button3 btn btn-success">Member Deals</a>
+            
+            </div>
+           
+        <!-- Navbar--> 
---
+++ index3.php
@@ -0,0 +1,20 @@
+<?php
+    include './Controller/login.php';
+    include './Controller/login-verify.php';    
+    include './Controller/login_button.php';
+    include './View/navbar.php';
+    include './Controller/db_conn.php';
+    ?>
+
+    <h1>Check back soon for more information!</h1>
+
+<!-- Footer -->
+<?php
+    include './View/footer.php';
+?>
+<!-- Footer -->
+
+    </div>
+</body>
+    
+</html>
